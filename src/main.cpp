ba018ae6a301bab6fc37db015570fb592caad5e3
WES3 <wfellis@btinternet.com>
2025-11-09 12:45:46 +0000

    moved spiffs into setup.
    added leds for output logging, will change to RGB led once we have one

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,15 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
+// --- Pin definitions ---
+#define RED_LED_PIN 25
+#define GREEN_LED_PIN 27
+#define BLUE_LED_PIN 26
+// #define BUTTON_PIN 0
+
 AsyncWebServer server(80);
 
-// Shared variables
+// --- Shared variables ---
 volatile bool wifiConnected = false;
 volatile bool startWiFiConnect = false;
 
@@ -16,6 +22,21 @@ String passwordInput = "";
 // Task handles
 TaskHandle_t WiFiTask;
 TaskHandle_t DataTask;
+
+void setupLEDs() {
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(GREEN_LED_PIN, OUTPUT);
+    pinMode(BLUE_LED_PIN, OUTPUT);
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, HIGH);
+    digitalWrite(BLUE_LED_PIN, HIGH);
+}
+
+void setLedColor(uint8_t red, uint8_t green, uint8_t blue) {
+    digitalWrite(RED_LED_PIN, red ? HIGH : LOW);
+    digitalWrite(GREEN_LED_PIN, green ? HIGH : LOW);
+    digitalWrite(BLUE_LED_PIN, blue ? HIGH : LOW);
+}
 
 // WiFi connection function (blocking, but called in task loop)
 void connectToWiFi() {
@@ -35,23 +56,22 @@ void connectToWiFi() {
         Serial.println(WiFi.localIP());
         wifiConnected = true;
         WiFi.softAPdisconnect(true);
+        setLedColor(0, 1, 0); // Green connected
     } else {
         Serial.println("\nFailed to connect.");
         wifiConnected = false;
+        setLedColor(1, 0, 0); // Red failed
     }
 }
 
 // WiFi Task - Core 1
 void WiFiTaskcode(void * pvParameter) {
-    if (!SPIFFS.begin(true)) {
-        Serial.println("Failed to mount SPIFFS");
-        vTaskDelete(NULL);
-        return;
-    }
+    setLedColor(1, 0, 0);  // Red initializing
 
     // Start AP
     WiFi.softAP("ESP32-Setup", "12345678");
     Serial.println("AP Started. Connect to WiFi 'ESP32-Setup' to configure");
+    setLedColor(0, 1, 0); // Green AP mode started
 
     // Setup async server
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -61,6 +81,7 @@ void WiFiTaskcode(void * pvParameter) {
 
     server.on("/connect", HTTP_POST, [](AsyncWebServerRequest *request) {
         if (request->hasParam("ssid", true) && request->hasParam("password", true)) {
+            setLedColor(1, 1, 0); // Yellow connecting
             ssidInput = request->getParam("ssid", true)->value();
             passwordInput = request->getParam("password", true)->value();
             startWiFiConnect = true;  // flag to connect in task loop
@@ -99,6 +120,15 @@ void DataTaskcode(void * pvParameter) {
 
 void setup() {
     Serial.begin(115200);
+
+    setupLEDs();
+    setLedColor(1, 0, 0);  // Red while setting up
+
+    if (!SPIFFS.begin(true)) {
+        Serial.println("Failed to mount SPIFFS");
+        setLedColor(0, 0, 1); // Blue
+        while(true) delay(1000); // halt here
+    }
 
     // Start tasks on separate cores
     xTaskCreatePinnedToCore(WiFiTaskcode, "WiFiTask", 12000, NULL, 1, &WiFiTask, 1);  // Core 1
